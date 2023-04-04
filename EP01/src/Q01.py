f1d82170a65b31d073e1216eeebeec80d69778ab
Patricia Duarte de Lima Machado <40029758+pdlmachado@users.noreply.github.com>
2023-04-04 16:29:51 -0300

    Update Q01.py

--- EP01/src/Q01.py
+++ EP01/src/Q01.py
@@ -3,5 +3,5 @@ import networkx as nx
 
 def associate_astronauts(list_a):  # Elementos da lista: (name,surname,country)
     # Escreva aqui o seu código
-    # return nx.Graph()
+    return nx.Graph()
 
